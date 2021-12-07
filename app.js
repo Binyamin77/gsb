4d8f848c0eed5d069ba42213d54fef701a52b8dd
Benjamin Hayat <benli1@hotmail.fr>
2021-12-07 12:30:46 +0100

    modif style.css et app.js et index.html

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
+const btnMenu = document.querySelector('.btn-rond-menu') /* Classe pour gérer le boutton */
+const nav = document.querySelector('.nav-gauche');
+const allItemNav = document.querySelectorAll('.nav-menu-item');  /* Classe pour gérer le menu */
+const ligne = document.querySelector('.cont-ligne'); /* Classe pour gérer les lignes */
+
+btnMenu.addEventListener('click', () => {  /* Dès qu'on clique sur le boutton, on va activer le toggle*/
+
+    ligne.classList.toggle('active')  /*  */
+})
 
 
