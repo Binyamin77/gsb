061663dfc722f1c61dda3dda3d37243673379dbe
Benjamin Hayat <benli1@hotmail.fr>
2021-12-08 16:43:56 +0100

    modif style.css et app.js

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ const ligne = document.querySelector('.cont-ligne'); /* Classe pour gérer les l
 btnMenu.addEventListener('click', () => {  /* Dès qu'on clique sur le boutton, on va activer le toggle*/
 
     ligne.classList.toggle('active')  /*  */
+    nav.classList.toggle('menu-visible')
 })
 
 
+if(window.matchMedia('(max-widith: 1300px)')) {  /*Si on est en dessous de 1300px */
+
+    allItemNav.forEach(item => {     /*Pour chaque élément */
+        item.addEventListener('click', () => {  /* Si on clique */
+            nav.classList.toggle('menu-visible')  /*Ca ferme le menu */
+            ligne.classList.toggle('active')  /* Ca remet l'icone comme elle était */
+        })
+    })
+
+}
