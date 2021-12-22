6d3eba9f6bf734e838da79d850f4d4c47620b63d
benli1@hotmail.fr= <benli1@hotmail.fr>
2021-12-22 18:24:46 +0100

    Ajout animations debut app.js

--- app.js
+++ app.js
@@ -20,3 +20,36 @@ if(window.matchMedia('(max-widith: 1300px)')) {  /*Si on est en dessous de 1300p
     })
 
 }
+
+//Animation écriture
+
+const txtAnim= document.querySelector('.txt-animation');
+
+let typewriter = new Typewriter(txtAnim, {
+    loop: true,
+    deleteSpeed: 20
+})
+
+
+typewriter
+    .pauseFor(1800)
+    .changeDelay(20)
+    .typeString('Moi c\'est Benjamin Hayat')
+    .pauseFor(300)
+    .typeString('<strong>, Développeur Web </strong> !')
+    .pauseFor(1000)
+    .deleteChars(19)
+    .typeString('<span style="color: #27ae60;"> HTML</span> !')
+    .pauseFor(1000)
+    .deleteChars(6)
+    .typeString('<span style="color: #EA39ff;"> Symfony </span> !')
+    .pauseFor(1000)
+    .deleteChars(10)
+    .typeString('<span style="color: midnightblue;">A completer </span> !')
+    .pauseFor(1000)
+    .deleteChars(15)
+    .typeString('<span style="color: #ff6910;"> A completer </span> !')
+    .start()
+
+
+
